9c8f490e1077d7e1f06bf18dbc236bba306b52bb
nlelab <161137745+nlelab@users.noreply.github.com>
2024-02-24 16:16:48 +0100

    encore du boulot pas comme Johan
    du vrai travail

--- monscript.ps1
+++ monscript.ps1
@@ -1,4 +1,3 @@
 Write-Host "I AM GAY"
-Write-Host  "JE ME CONNECTE A UNE APPLI AVEC LE MDP LELE (PAS DE MDP EN CLAIR FDP) : XXXX"
-Write-Host "je sabote le projet"
-Write-Host "je ruine vraiment le projet"
+Write-Host "JE ME CONNECTE A UNE APPLI AVEC LE MDP LELE (PAS DE MDP EN CLAIR FDP) : XXXX"
+Write-Host "plus de commandes"
